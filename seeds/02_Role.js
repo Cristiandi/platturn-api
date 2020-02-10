9e9afd53f26dc81b5e74258c0c63d7008998ab2a
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-02-09 22:23:20 -0500

    feat(api): base seeds

---
+++ seeds/02_Role.js
@@ -0,0 +1,45 @@
+
+const seed = async knex => {
+  const roles = [
+    {
+      id: 1,
+      name: 'Desarrollador',
+      code: 'RO01'
+    },
+    {
+      id: 2,
+      name: 'Administrador',
+      code: 'RO02'
+    },
+    {
+      id: 3,
+      name: 'Usuario',
+      code: 'RO03'
+    },
+    {
+      id: 4,
+      name: 'Usuario basico',
+      code: 'RO04'
+    }
+  ];
+
+  const promises = roles.map(async role => {
+    const rows = await knex('Role').select('id').where('code', role.code);
+    if (!rows.length) {
+      const [id] = await knex('Role').insert(role);
+      return { id };
+    } else {
+      await knex('Role')
+        .update({ ...role })
+        .where({
+          code: role.code
+        });
+    }
+  });
+
+  await Promise.all(promises);
+};
+
+module.exports = {
+  seed
+};
---
+++ seeds/03_SheduleType.js
@@ -0,0 +1,37 @@
+
+const seed = async knex => {
+  const sheduleTypes = [
+    {
+      id: 1,
+      name: 'Mensual',
+      code: 'ST01',
+      numberOfDays: 30
+    },
+    {
+      id: 2,
+      name: 'Quincenal',
+      code: 'ST02',
+      numberOfDays: 15
+    }
+  ];
+
+  const promises = sheduleTypes.map(async sheduleType => {
+    const rows = await knex('SheduleType').select('id').where('code', sheduleType.code);
+    if (!rows.length) {
+      const [id] = await knex('SheduleType').insert(sheduleType);
+      return { id };
+    } else {
+      await knex('SheduleType')
+        .update({ ...sheduleType })
+        .where({
+          code: sheduleType.code
+        });
+    }
+  });
+
+  await Promise.all(promises);
+};
+
+module.exports = {
+  seed
+};
